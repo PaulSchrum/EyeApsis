02beda64428fb08f68e8dda213bc0a8e72d0fd24
Paul Schrum <paul.schrum@gmail.com>
2015-02-11 19:01:48 -0500

    WIP: Chart Window now appears with Calibration Screen

--- EyeApsisApp/App.xaml.cs
+++ EyeApsisApp/App.xaml.cs
@@ -29,33 +29,48 @@ namespace EyeApsisApp
       public void openChartWindowOnTheCorrectScreen()
       {
          ((Grid)chartWindow.Content).DataContext = chartVM;
+         ((Grid)calibrateScreenSizeWindow.Content).DataContext = chartVM;
          calibrateScreenSizeWindow.Show();
-      //   chartWindow.DataContext = this.DataContext;
-      //   chartVM.VerticalCalibration.AdjustmentMultiplier =
-      //      Convert.ToDouble(Properties.Settings.Default.VerticalAdjustmentFactor);
+         chartWindow.DataContext = chartVM;
+         chartVM.VerticalCalibration.AdjustmentMultiplier = 1.0;
+         
+         //chartVM.VerticalCalibration.AdjustmentMultiplier =
+         //   Convert.ToDouble(Properties.Settings.Default.VerticalAdjustmentFactor);
 
-      //   if (this.IsSingleScreen == false)
-      //   {
-      //      if (SystemParameters.VirtualScreenLeft < 0.0)
-      //      { // get the window onto the secondary screen (left screen)
-      //         chartWindow.Left = SystemParameters.VirtualScreenLeft + 20;
-      //      }
-      //      else
-      //      {   // get the window onto the secondary screen (right screen)
-      //         chartWindow.Left =
-      //            SystemParameters.MaximizedPrimaryScreenWidth + 20;
-      //      }
-      //      chartWindow.Topmost = true;
-      //   }
-      //   else //single screen, no changes neccessary to window location
-      //   {  // but lets move it a little anyway so it is not on top
-      //      // of the main window
-      //      chartWindow.Left = this.Left + 96;
-      //      chartWindow.Topmost = false;
-      //      this.Topmost = true;
-      //   }
+         if (this.IsSingleScreen == false)
+         {
+            if (SystemParameters.VirtualScreenLeft < 0.0)
+            { // get the window onto the secondary screen (left screen)
+               chartWindow.Left = SystemParameters.VirtualScreenLeft + 20;
+            }
+            else
+            {   // get the window onto the secondary screen (right screen)
+               chartWindow.Left =
+                  SystemParameters.MaximizedPrimaryScreenWidth + 20;
+            }
+            chartWindow.Topmost = true;
+         }
+         else //single screen, no changes neccessary to window location
+         {  // but lets move it a little anyway so it is not on top
+            // of the main window
+            chartWindow.Left = calibrateScreenSizeWindow.Left + 96;
+            chartWindow.Topmost = false;
+            calibrateScreenSizeWindow.Topmost = true;
+         }
 
+         chartWindow.Show();
+         chartWindow.WindowState = System.Windows.WindowState.Maximized;
+      }
 
+      protected bool IsSingleScreen
+      {
+         get
+         {
+            return
+               (Math.Abs(
+               SystemParameters.PrimaryScreenWidth - SystemParameters.VirtualScreenWidth) < 2.0);
+         }
+         private set { }
       }
 
    }
