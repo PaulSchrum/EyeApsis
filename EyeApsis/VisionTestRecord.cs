ca270177070b461529d89b47566915df9dd82501
Paul Schrum <paul.schrum@gmail.com>
2015-02-18 14:01:29 -0500

    Added Vision Test Record class

---
+++ EyeApsis/VisionTestRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeApsis
+{
+   class VisionTestRecord
+   {
+      String SubjectFirstName;
+      String SubjectLastName;
+      DateTime TestDateAndTime;
+      String TestLocation;
+      String LeftEyeSurgicalAcuityCorrection;
+      String LeftEyeCorrection;
+      int LeftEyeVisualAcuity;
+      String RightEyeSurgicalAcuityCorrection;
+      String RightEyeCorrection;
+      int RightEyeVisualAcuity;
+      int ColorSensitivity;
+      int LeftEyeContrastSensitivity;
+      int RightEyeContrastSensitivity;
+      
+   }
+}
